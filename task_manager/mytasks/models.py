20bb9a5d583d15d402a30d07a9e0e4c5f68545d0
dmifi <58224767+dmifi@users.noreply.github.com>
2020-08-18 21:26:05 +0300

    make admin

--- task_manager/mytasks/models.py
+++ task_manager/mytasks/models.py
@@ -1,21 +1,39 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 
 class User(models.Model):
-    nickname = models.CharField('Имя комментатора', max_length=100)
-    first_name = models.CharField('Имя комментатора', max_length=100)
-    second_name = models.CharField('Имя комментатора', max_length=100)
+    nickname = models.CharField('Никнейм', max_length=100)
+    first_name = models.CharField('Имя', max_length=100)
+    last_name = models.CharField('Фамилия', max_length=100)
+
+    def __str__(self):
+        return self.nickname
 
 
 class Task(models.Model):
     task_title = models.CharField('Название задачи', max_length=100)
     task_text = models.TextField('Текст задачи')
-    pub_date = models.DateTimeField('Дата постановки задачи')
+    created = models.DateTimeField('Дата постановки задачи', auto_now_add=True)
+    updated = models.DateTimeField('Обновлено', auto_now=True)
+
+    def __str__(self):
+        return self.task_title
+
+    # def today_task(self):
+    #     return self.created >= (timezone.now() - datetime.timedelta(days=1))
+
+    class Meta:
+        ordering = ('-created',)
 
 
 class Comment(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    nickname = models.ForeignKey(User, verbose_name="Никнейм", on_delete=models.CASCADE)
+    comment_to_task = models.ForeignKey(Task, verbose_name="Задача", on_delete=models.CASCADE)
     comment_text = models.CharField('Текст комментария', max_length=200)
-    nickname = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment_text
