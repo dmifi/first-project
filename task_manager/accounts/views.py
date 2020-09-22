5012995ac9676b55c033760f84dc7a73d5a263eb
dmifi <58224767+dmifi@users.noreply.github.com>
2020-09-22 20:06:53 +0300

    add accounts/views

--- task_manager/accounts/views.py
+++ task_manager/accounts/views.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .forms import LoginForm
+
+
+class LoginView(View):
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request,
+                username = cd['username'],
+                password = cd['password']
+            )
+            if user is None:
+                return HttpResponse('Неправильный логин и/или пароль')
+
+            if not user.is_active:
+                return HttpResponse('Ваш аккаунт заблокирвоан')
+
+            login(request, user)
+            return HttpResponse('Добро пожаловать')
+
+        return render(request, 'accounts/login.html', {'form': form})
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm()
+        return render(request, 'accounts/login.html', {'form': form})
+
--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'mytasks.apps.MytasksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
